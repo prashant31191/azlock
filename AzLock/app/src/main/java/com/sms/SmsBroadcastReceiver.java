86d18e0b289be8860bd7305c21fa5fdf682f4408
Prashant <prashant.chovatiya@eliteinfoworld.com>
2017-10-30 14:10:55 +0530

    Initial commit

---
+++ AzLock/app/src/main/java/com/sms/SmsBroadcastReceiver.java
@@ -0,0 +1,39 @@
+package com.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * A broadcast receiver who listens for incoming SMS
+ */
+public class SmsBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "SmsBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+            String smsSender = "";
+            String smsBody = "";
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
+                for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                    smsBody += smsMessage.getMessageBody();
+                }
+
+            }
+
+            if (smsBody.startsWith(SmsHelper.SMS_CONDITION)) {
+                Log.d(TAG, "Sms with condition detected");
+                Toast.makeText(context, "BroadcastReceiver caught conditional SMS: " + smsBody, Toast.LENGTH_LONG).show();
+            }
+            Log.d(TAG, "SMS detected: From " + smsSender + " With text " + smsBody);
+        }
+    }
+}
